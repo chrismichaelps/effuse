27f0f85fd8cfedad05a1a5c3a7aca843aa843073
Chris Michael <chris.perez@upr.edu>
2025-12-21 17:50:15 -0400

    feat: improve store devtools logging.

--- packages/store/src/core/store.ts
+++ packages/store/src/core/store.ts
@@ -178,8 +178,8 @@ export const createStore = <T extends object>(
 		set(_, prop: string, value: unknown) {
 			if (!internals.signalMap.has(prop)) return false;
 
-			const sig = internals.signalMap.get(prop)!;
-			const oldValue = sig.value;
+			const sig = internals.signalMap.get(prop);
+			if (!sig) return false;
 			const newState = middlewareManager.execute(
 				{ ...atomicState.get(), [prop]: value },
 				`set:${prop}`,
@@ -190,7 +190,27 @@ export const createStore = <T extends object>(
 			atomicState.update((s) => ({ ...s, [prop]: newState[prop] }));
 
 			if (enableDevtools) {
-				console.log(`[${name}] ${prop}:`, oldValue, '->', newState[prop]);
+				const time = new Date().toLocaleTimeString();
+				console.groupCollapsed(
+					`%caction %c${name}/set:${prop} %c@ ${time}`,
+					'color: gray; font-weight: lighter;',
+					'color: inherit; font-weight: bold;',
+					'color: gray; font-weight: lighter;'
+				);
+				console.log(
+					'%cprev state',
+					'color: #9E9E9E; font-weight: bold;',
+					atomicState.get()
+				);
+				console.log('%caction', 'color: #03A9F4; font-weight: bold;', {
+					type: `set:${prop}`,
+					payload: value,
+				});
+				console.log('%cnext state', 'color: #4CAF50; font-weight: bold;', {
+					...atomicState.get(),
+					[prop]: newState[prop],
+				});
+				console.groupEnd();
 			}
 
 			notifySubscribers();
@@ -204,9 +224,9 @@ export const createStore = <T extends object>(
 	const boundActions: Record<string, (...args: unknown[]) => unknown> = {};
 	for (const [key, action] of Object.entries(internals.actions)) {
 		boundActions[key] = (...args: unknown[]) => {
-			if (enableDevtools) {
-				console.log(`[${name}] ${key}(`, ...args, ')');
-			}
+			const prevState = enableDevtools
+				? getSnapshot(internals.signalMap)
+				: undefined;
 
 			const existingToken = internals.pendingActions.get(key);
 			if (existingToken) {
@@ -225,6 +245,32 @@ export const createStore = <T extends object>(
 							internals.pendingActions.delete(key);
 							const currentState = getSnapshot(internals.signalMap);
 							middlewareManager.execute(currentState, key, args);
+
+							if (enableDevtools) {
+								const time = new Date().toLocaleTimeString();
+								console.groupCollapsed(
+									`%caction %c${name}/${key} (async) %c@ ${time}`,
+									'color: gray; font-weight: lighter;',
+									'color: inherit; font-weight: bold;',
+									'color: gray; font-weight: lighter;'
+								);
+								console.log(
+									'%cprev state',
+									'color: #9E9E9E; font-weight: bold;',
+									prevState
+								);
+								console.log('%caction', 'color: #03A9F4; font-weight: bold;', {
+									type: `${name}/${key}`,
+									payload: args,
+								});
+								console.log(
+									'%cnext state',
+									'color: #4CAF50; font-weight: bold;',
+									currentState
+								);
+								console.groupEnd();
+							}
+
 							notifySubscribers();
 						}
 						return value;
@@ -237,6 +283,31 @@ export const createStore = <T extends object>(
 
 			const currentState = getSnapshot(internals.signalMap);
 			middlewareManager.execute(currentState, key, args);
+
+			if (enableDevtools) {
+				const time = new Date().toLocaleTimeString();
+				console.groupCollapsed(
+					`%caction %c${name}/${key} %c@ ${time}`,
+					'color: gray; font-weight: lighter;',
+					'color: inherit; font-weight: bold;',
+					'color: gray; font-weight: lighter;'
+				);
+				console.log(
+					'%cprev state',
+					'color: #9E9E9E; font-weight: bold;',
+					prevState
+				);
+				console.log('%caction', 'color: #03A9F4; font-weight: bold;', {
+					type: `${name}/${key}`,
+					payload: args,
+				});
+				console.log(
+					'%cnext state',
+					'color: #4CAF50; font-weight: bold;',
+					currentState
+				);
+				console.groupEnd();
+			}
 
 			notifySubscribers();
 
