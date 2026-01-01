1342485c25c6450550be3109b111833952bb9225
Chris Michael <chris.perez@upr.edu>
2026-01-01 17:41:04 -0400

    feat(app): add reusable toggle, scroll spy, and click outside hooks

---
+++ app/src/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useScrollSpy } from './useScrollSpy.js';
+export { useAnimatedDropdown } from './useAnimatedDropdown.js';
+export { useClickOutside } from './useClickOutside.js';
+export { useToggle } from './useToggle.js';
+export { useInfiniteScroll } from './useInfiniteScroll.js';
